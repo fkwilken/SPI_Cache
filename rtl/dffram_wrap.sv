209e0535b321757e2ce0c7606cef45a49dab33de
fkwilken <fkwilken@gmail.com>
2025-03-06 01:03:30 +0100

    Add dffram wrapper thing
    Signed-off-by: fkwilken <fkwilken@gmail.com>

---
+++ rtl/dffram_wrap.sv
@@ -0,0 +1,16 @@
+module dffram_wrap (clk_i,
+    EN0,
+    A0,
+    Di0,
+    Do0,
+    WE0);
+ input clk_i;
+ input EN0;
+ input [7:0] A0;
+ input [15:0] Di0;
+ output [15:0] Do0;
+ input [1:0] WE0;
+
+RAM256 dffram0 (.CLK(clk_i), .*);
+
+endmodule
